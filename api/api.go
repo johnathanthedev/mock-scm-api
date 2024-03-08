cf626ba7e099cf3e5c12ecdb96895dffd81b4962
Johnathan Guzman <johnathanthedev@gmail.com>
2024-03-08 15:56:03 -0600

    (Enhancement) Add CORS support

--- api/api.go
+++ api/api.go
@@ -1,13 +1,21 @@
 package api
 
 import (
+	"os"
 	"scm-api/api/validator"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func StartServer() error {
 	e := echo.New()
+
+	frontend_url := os.Getenv("FRONTEND_URL")
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{frontend_url},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
 
 	cv := validator.Init()
 
--- go.mod
+++ go.mod
@@ -15,8 +15,10 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/paulmach/orb v0.11.1 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
 
 require (
