488c5fab5cb22f02c0588a464b37be1ce69d8924
Johnathan Guzman <45727071+johnathanthedev@users.noreply.github.com>
2024-03-22 10:17:36 -0500

    (Enhancement) Retrieve port from env (#40)

--- api/api.go
+++ api/api.go
@@ -14,6 +14,11 @@ func StartServer(broker *ws.Broker) error {
 	e := echo.New()
 
 	frontend_url := os.Getenv("FRONTEND_URL")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{frontend_url},
 		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
@@ -23,5 +28,5 @@ func StartServer(broker *ws.Broker) error {
 
 	InitRoutes(e, cv, broker)
 
-	return e.Start(":8080")
+	return e.Start(":" + port)
 }
